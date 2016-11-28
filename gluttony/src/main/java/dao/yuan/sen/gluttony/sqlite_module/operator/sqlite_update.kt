6c295adab214e58f32d2ad0b43af1c10e56f4aec
森与渊 <970865686@qq.com>
2016-11-28 21:47:41 +0800

    update添加类实例操作模式 代码分为realm和sqlite模块

--- gluttony/src/main/java/dao/yuan/sen/gluttony/sqlite_module/operator/sqlite_update.kt
+++ gluttony/src/main/java/dao/yuan/sen/gluttony/sqlite_module/operator/sqlite_update.kt
@@ -15,7 +15,7 @@ import kotlin.reflect.declaredMemberProperties
 
 /**
  * 根据 实例 更新数据 ， 依靠实例的主键定位，如果数据库中不存在的话，将保存数据
- * 返回值：更新的数量位1 或 保存数据的id*/
+ * @return 更新的数量位1 或 保存数据的id*/
 inline fun <reified T : Any> T.updateOrSave(): Long {
     return if (update() == 0) save()
     else 1
@@ -29,7 +29,7 @@ inline fun <reified T : Any> T.updateByKey(primaryKey: Any, updateFunctor: (T) -
 
 /**
  * 根据 实例 更新数据 ， 依靠实例的主键定位
- * 返回值：更新的数量*/
+ * @return 更新的数量*/
 inline fun <reified T : Any> T.update(): Int {
     val mClass = this.javaClass.kotlin
     val properties = mClass.declaredMemberProperties
@@ -58,13 +58,13 @@ inline fun <reified T : Any> T.update(): Int {
 
 /**
  * 根据 主键 更新数据
- * 返回值：更新的数量*/
+ * @return 更新的数量*/
 inline fun <reified T : Any> T.updateByKey(primaryKey: Any, crossinline pairs: () -> Array<out Pair<String, Any>>): Int = updateByKey(primaryKey, *pairs())
 
 
 /**
  * 根据 主键 更新数据
- * 返回值：更新的数量*/
+ * @return 更新的数量*/
 inline fun <reified T : Any> T.updateByKey(primaryKey: Any, vararg pairs: Pair<String, Any>): Int {
     val mClass = this.javaClass.kotlin
     val name = "${mClass.simpleName}"
@@ -89,7 +89,7 @@ inline fun <reified T : Any> T.updateByKey(primaryKey: Any, vararg pairs: Pair<S
 }
 
 
-/**返回值：更新的数量*/
+/**@return 更新的数量*/
 inline fun <reified T : Any> T.update(vararg pairs: Pair<String, Any>, crossinline condition: UpdateQueryBuilder.() -> Unit): Int {
     val name = "${this.javaClass.kotlin.simpleName}"
     return Gluttony.database.use {
